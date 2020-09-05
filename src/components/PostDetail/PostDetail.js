dc1d5508973c3ccbcbdbaab9fbae083de94f42b0
najmus2808 <najmus2808@gmail.com>
2020-09-04 22:08:39 -0700

    message section with image work is done!

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 const PostDetail = () => {
   let { id } = useParams();
-
   const [post, setPost] = useState({});
   const { title, body } = post;
 
@@ -15,13 +20,21 @@ const PostDetail = () => {
   }, [id]);
 
   const [comments, setComments] = useState([]);
-  const { name, } = post;
-
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/comments?postId=${id}`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setComments(data));
+  }, [id]);
+
+  const [images, setImages] = useState([]);
+
+
+  useEffect(() => {
+    const url = `https://loremflickr.com/600/400?random=${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setImages(data));
   }, [id]);
 
   const postStyle = {
@@ -30,6 +43,22 @@ const PostDetail = () => {
     padding:'20px',
     borderRadius:'10px'
 }
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+
+  const classes = useStyles();
+
+
+
   return (
     <div>
         <div style={postStyle}>
@@ -38,11 +67,31 @@ const PostDetail = () => {
       <h3>Description: {body}</h3>
     </div>
 
-    <div style={postStyle}>
-         <h1>Comment Section</h1>
+    <div>
+         
 
          {
-                comments.map(comment => <h2>{comment.name}</h2>)
+                comments.map(comment => 
+                  
+                  <Card className={classes.root}>
+                  <CardActionArea>
+                    <CardMedia
+                      className={classes.media}
+                      image={`https://loremflickr.com/600/400?random=${id}`}
+                      title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {comment.name}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p">
+                        {comment.body}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+                  
+                  )
          }
     </div>
     </div>
