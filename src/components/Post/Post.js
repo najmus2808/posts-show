6215073575f983de53ce308eb23b23343fa593d7
najmus2808 <najmus2808@gmail.com>
2020-09-05 14:35:51 -0700

    some change in design part

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const Post = (props) => {
-    const {id, title} = props.post;
+  const { id, title } = props.post;
 
-    const postStyle = {
-        border:'1px solid blue',
-        margin:'20px',
-        padding:'20px',
-        borderRadius:'10px'
-    }
-    return (
-        <div style={postStyle}>
-            <h1>Title: {title}</h1>
-            <Link to={`/post/${id}`}>
-            <button>See Detail</button>
-            </Link>
-            
-        </div>
-    );
+  const linkStyle = {
+    textDecoration:'none'
+  };
+
+  const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: "inline-block",
+      margin: "0 2px",
+      transform: "scale(0.8)",
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+  const classes = useStyles();
+  
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          Post: {id}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Title: {title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Link style={linkStyle}  to={`/post/${id}`}>
+          <Button variant="contained" color="primary">
+            See Detail
+          </Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
 const PostDetail = () => {
   let { id } = useParams();
   const [post, setPost] = useState({});
@@ -20,48 +25,72 @@ const PostDetail = () => {
       .then((data) => setComments(data));
   }, [id]);
 
+  
+  
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
+
+  const classes = useStyles();
+  
   
 
-  const postStyle = {
-    border:'1px solid blue',
-    margin:'20px',
-    padding:'20px',
-    borderRadius:'10px'
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+         Post Details
+        </Typography>
+        <Typography className={classes.title} color="textSecondary">
+          Title: {post.title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          Description: {post.body}
+        </Typography>
+      </CardContent>
+<hr/>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+         Comment Section
+        </Typography>
+        <Typography className={classes.title} color="textSecondary">
+        {comments.map((comment) => (
+          <div>
+           <div style={{display:'flex'}}>
+           <img style={{borderRadius:'50%', padding:'10px'}} src={`https://loremflickr.com/600/400?random=${comment.id}`}
+              alt=""
+              height="50"
+              width="50"
+            />
+            <h3 style={{padding:'10px' }}>{comment.name}</h3>
+           </div>
 
-  }
-
-  const comStyle = {
-    textAlign:'center',
-    color:'red'
-
-  }
-
-
-
-
-    return (
-      <div>
-          <div style={postStyle}>
-          <h1 style={comStyle}><strong>Post Details</strong></h1>
-    <h1>Id: {post.id}</h1>
-    <h2>Title: {post.title}</h2>
-    <h3>Description: {post.body}</h3>
-    <hr/>
-
-    <h1 style={comStyle}><strong>Comment Section</strong></h1>
-
-
-    {
-      comments.map(comment => 
-      <div>
-        <img src={ `https://loremflickr.com/600/400?random=${comment.id}`} alt="" height="100" width="100"/>
-      <h3>{comment.body}</h3>
-      <hr/>
-      </div>)
-    }
+            <div>
+            {comment.body}
+            <hr/>
           </div>
-      </div>
+
+          </div>
+        ))}
+        </Typography>
+      </CardContent>
+      
+      
+    </Card>
   );
 };
 
