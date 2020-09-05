d2f3f234b263f753fd744d1d94077e35d331bede
najmus2808 <najmus2808@gmail.com>
2020-09-05 04:44:53 -0700

    a little change in post detail component

--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 
 const PostDetail = () => {
   let { id } = useParams();
   const [post, setPost] = useState({});
-  const { title, body } = post;
 
   useEffect(() => {
     const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
@@ -27,8 +20,8 @@ const PostDetail = () => {
       .then((data) => setComments(data));
   }, [id]);
 
-  const [images, setImages] = useState([]);
 
+  const [images, setImages] = useState();
 
   useEffect(() => {
     const url = `https://loremflickr.com/600/400?random=${id}`;
@@ -42,59 +35,34 @@ const PostDetail = () => {
     margin:'20px',
     padding:'20px',
     borderRadius:'10px'
-}
 
+  }
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
+  const comStyle = {
+    textAlign:'center',
+    color:'red'
 
+  }
 
-  const classes = useStyles();
+    return (
+      <div>
+          <div style={postStyle}>
+          <h1 style={comStyle}><strong>Post Details</strong></h1>
+    <h1>Id: {post.id}</h1>
+    <h2>Title: {post.title}</h2>
+    <h3>Description: {post.body}</h3>
+    <hr/>
 
+    <h1 style={comStyle}><strong>Comment Section</strong></h1>
 
-
-  return (
-    <div>
-        <div style={postStyle}>
-      <h1>Id: {id}</h1>
-      <h2>Title: {title}</h2>
-      <h3>Description: {body}</h3>
-    </div>
-
-    <div>
-         
-
-         {
-                comments.map(comment => 
-                  
-                  <Card className={classes.root}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.media}
-                      image={`https://loremflickr.com/600/400?random=${id}`}
-                      title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {comment.name}
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary" component="p">
-                        {comment.body}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
-                  
-                  )
-         }
-    </div>
-    </div>
+    {
+      comments.map(comment => <div>
+        <img src={images} alt="" height="200" width="200"/>
+      <h3>{comment.body}</h3>
+      </div>)
+    }
+          </div>
+      </div>
   );
 };
 
